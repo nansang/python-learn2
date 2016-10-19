4fb856929e2e6c80209f46b61813a4200fbbc692
hackMarshal <815088568@qq.com>
2016-10-19 21:57:16 +0800

    爬虫，算法，装饰器

---
+++ 爬虫/index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+'''
+Created on:
+@author:
+Email:
+Version:
+Description:
+Help:http://www.cnblogs.com/wupeiqi/articles/5354900.html
+'''
+
+'''
+Requests库
+自动登陆抽屉并点赞
+"破解"微信公众号
+Scrapy
+'''
+#感觉这才有搞头
---
+++ 装饰器/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+'''
+Created on:
+@author:
+Email:
+Version:
+Description:
+Help:http://www.cnblogs.com/wupeiqi/articles/4980620.html
+'''
+
+'''
+行业人员的解释是对函数附加功能，但不影响原函数，比如是对cookie的判断什么的
+'''
